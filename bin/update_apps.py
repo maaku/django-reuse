e7c9adc52422346283e4f07bf5a0a7b8e99f2e8e
Mark Friedenbach <mark_friedenbach@yahoo.com>
2009-04-02 14:58:30 -0700

    Added python linking scripts and update_apps.py

---
+++ bin/update_apps.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##
+# django-reuse: bin/update_apps.py
+##
+
+import os
+import os.path
+from subprocess import call
+
+if __name__ == "__main__":
+    apps_dir = os.path.abspath('.')
+    for app_name in os.listdir(apps_dir):
+        app_dir = os.path.abspath(os.path.join(apps_dir, app_name))
+        git_path = os.path.join(app_dir, '.git')
+        svn_path = os.path.join(app_dir, '.svn')
+        if os.path.lexists(svn_path):
+            print "Updating svn %s" % app_dir
+            os.chdir(app_dir)
+            call(['svn', 'update'])
+        elif os.path.lexists(git_path):
+            print "Updating git %s" % app_dir
+            os.chdir(app_dir)
+            call(['git', 'pull'])
+        else:
+            continue
+
+##
+# End of File
+##
