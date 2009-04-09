dea467bc211e6b9d3c6b4cb707b019652a0607fd
Mark Friedenbach <mark_friedenbach@yahoo.com>
2009-04-09 16:49:34 -0700

    streamlined update code, and added simple command line parser and error code

--- bin/reuse/update.py
+++ bin/reuse/update.py
@@ -9,44 +9,53 @@
 docstring for update.py
 """
 
-import sys
-import getopt
-
+##
+# Boilerplate
 import os
-import os.path
-from subprocess import call
+import sys
+script     = os.path.basename(sys.argv[1])
+subcommand = os.path.basename(sys.argv[0])
+subcommand = subcommand[:-len(".py")]
+del sys.argv[1]
 
-def update():
+ARGS = { "script":     script,
+         "subcommand": subcommand }
+
+from subprocess import call
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        for opt in sys.argv[1:]:
+            ARGS["opt"] = opt
+            print ("""
+%(subcommand)s: unrecognized option \'%(opt)s\'.
+"""% ARGS).strip()
+        print ("""
+%(subcommand)s: try \'%(script)s help %(subcommand)s\' for more information.
+"""% ARGS).strip(); sys.exit(-1)
+
     apps_dir = os.path.abspath('.')
     for app_name in os.listdir(apps_dir):
         app_dir = os.path.abspath(os.path.join(apps_dir, app_name))
-        git_path = os.path.join(app_dir, '.git')
-        svn_path = os.path.join(app_dir, '.svn')
-        mtn_path = os.path.join(app_dir, '_MTN')
-        hg_path  = os.path.join(app_dir, '.hg')
-        if os.path.lexists(svn_path):
+        if os.path.lexists(os.path.join(app_dir,'.svn')):
             print "Updating svn %s" % app_dir
             os.chdir(app_dir)
             call(['svn', 'update'])
-        elif os.path.lexists(git_path):
+        elif os.path.lexists(os.path.join(app_dir,'.git')):
             print "Updating git %s" % app_dir
             os.chdir(app_dir)
             call(['git', 'pull'])
-        elif os.path.lexists(mtn_path):
+        elif os.path.lexists(os.path.join(app_dir,'_MTN')):
             print "Updating mtn %s" % app_dir
             os.chdir(app_dir)
             call(['mtn', 'pull'])
             call(['mtn', 'update'])
-        elif os.path.lexists(hg_path):
+        elif os.path.lexists(os.path.join(app_dir,'.hg')):
             print "Updating hg %s" % app_dir
             os.chdir(app_dir)
             call(['hg', 'pull'])
             call(['hg', 'update'])
         else:
             continue
-
-if __name__ == "__main__":
-    pass
 
 ##
 # End of File
