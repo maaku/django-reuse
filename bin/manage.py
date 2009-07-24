0476e12acacc26255c444655413fde5ffb5a5361
Mark Friedenbach <mfrieden@odin000956054.(none)>
2009-07-24 10:22:06 -0700

    cleaned up code comments and removed old, commented out code

--- bin/manage.py
+++ bin/manage.py
@@ -53,8 +53,10 @@ if __name__ == "__main__":
  dev_root    = os.path.join(script_dir,'..','..')
 
 ##
-# FIXME: add the django-extensions module to the python path, and then apply
-# our own tweaks to the environment.
+# Django command extensions are used to extend the capability of Django's
+# built-in django-admin.py.  By default we use a suite of community provided
+# extensions, as well as our own extensions targetted at reusable application
+# development.
 
  import sys
 
@@ -79,7 +81,7 @@ if __name__ == "__main__":
 # that this script is being called from a development environment th
 # resembles one created by the bootstrap script.
 
- dev_dirs    = [x for x in os.listdir(dev_root) if os.path.isdir(x)]
+ dev_dirs = [x for x in os.listdir(dev_root) if os.path.isdir(x)]
  # look for a final build...
  for dir in [x for x in dev_dirs if "final" in x]:
      sys.path.insert(0,dir)
@@ -92,147 +94,6 @@ if __name__ == "__main__":
  for dir in [x for x in dev_dirs if "trunk" in x]:
      sys.path.insert(0,dir)
      try_execute_from_cmdline()
-
-#
-#import os
-#import sys
-#script      = os.path.realpath(__file__)
-#script_base = script[:-len(".py")]
-#sys.path.insert(0,script_base)
-#
-#from django.core import managemen
-#
-#if __name__ == "__main__":
-#    management.execute_from_command_line()
-
-# ##
-# # The license string is read from the file COPYING.txt in the root of the
-# # django-reuse project directory.  The contents of this file is output in
-# # response to the user specified '--license' option.
-# licname = os.path.join(script_base,"..","..","COPYING.txt")
-# try:
-#     licfile = open(licname, 'r')
-#     license = licfile.read()
-#     licfile.close()
-# except:
-#     license = "Error reading license file %s..." % licname
-#
-# ##
-# # The functions usage(), help(), version(), and license() handle the user
-# # specified options '-h' and '--usage', '--help', '--version', and '--license'
-# # respectfully.
-# ARGS = { "basename":  os.path.basename(sys.argv[0]),
-#          "copyright": COPYRIGHT,
-#          "version":   VERSION_LONG,
-#          "license":   license }
-#
-# def usage():
-#     print ("""
-# Usage: %(basename)s <command> [options]
-# Try \'%(basename)s --help\' for more information.
-# """% ARGS).strip(); sys.exit(0)
-#
-# def help():
-#     print ("""
-# Usage: %(basename)s <command> [options]
-#
-# Options:
-#    -h        Display a short usage statement and exit.  All other arguments
-#   --usage    except \'--help\' are ignored.
-#
-#   --help     Display this help message and exit.  All other arguments are
-#              ignored.
-#
-#   --version  Display copyright and version information and then exit.  All
-#              other arguments except \'-h\'/\'--usage\', \'--help\', and \'--license\'
-#              are ignored.
-#
-#   --license  Display copyright and license then exit.  All other arguments
-#              except \'-h\'/\'--usage\' and \'--help\' are ignored.
-#
-# If \'-h\', \'--usage\', \'--help\', \'--version\', or \'--license\' is specified, no
-# action is performed, regardless of any other parameters that may also be
-# specified.
-#
-# The following commands are recognized (use \'%(basename)s help <command>\'
-# to get detailed documentation of each command):
-# """% ARGS).strip();
-#     import re
-#     print ""
-#     for filename in os.listdir(script_base):
-#         if not re.match(r"^.*.py$", filename) or filename == "__init__.py":
-#             continue
-#         print "    %s" % filename[:-len(".py")]
-#     sys.exit(0)
-#
-# def version():
-#     print ("""
-# %(basename)s - Django Reusability Tool - version %(version)s
-# Copyright (c) %(copyright)s by its contributors and distributed
-# under the terms of the GNU Affero General Public License, version 3
-# Try \'%(basename)s --license\' for more information.
-# """% ARGS).strip(); sys.exit(0)
-#
-# def license():
-#     print ("""
-# %(basename)s - Django Reusability Tool - version %(version)s
-# Copyright (c) %(copyright)s by its contributors and distributed
-# under the terms of the GNU Affero General Public License, version 3
-#
-# %(license)s
-# """% ARGS).strip(); sys.exit(0)
-#
-# ##
-# #　Handle basic usage- and info-related commands.
-# opt_h       = False
-# opt_help    = False
-# opt_version = False
-# opt_license = False
-# for opt in sys.argv[1:]:
-#     if opt=="-h":        opt_h       = True
-#     if opt=="--usage":   opt_h       = True
-#     if opt=="--help":    opt_help    = True
-#     if opt=="--version": opt_version = True
-#     if opt=="--license": opt_license = True
-#
-# if opt_help:    help()
-# if opt_h:       usage()
-# if opt_license: license()
-# if opt_version: version()
-#
-# ##
-# # Extract command, removing it from the list of arguments.
-# cmd = None
-# for arg in sys.argv[1:]:
-#     if arg.startswith("-") != True:
-#         cmd = arg.strip().lower()
-#         sys.argv.remove(arg)
-#         break
-#
-# ##
-# # See if there is anything to do
-# if cmd == None:
-#     print ("""
-# %(basename)s: nothing to do; exiting.
-# %(basename)s: try \'%(basename)s --help\' for usage information.
-# """% ARGS).strip(); sys.exit(0)
-#
-# ##
-# # See if command is valid (subscript exists)
-# subscript = os.path.join(script_base, "".join([cmd,".py"]))
-# if os.path.exists(subscript) != True:
-#     ARGS["cmd"]=cmd
-#     print ("""
-# %(basename)s: unrecognized subcommand \'%(cmd)s\'.
-# %(basename)s: try \'%(basename)s --help\' for a list of valid commands.
-# """% ARGS).strip(); sys.exit(-1)
-#
-# ##
-# # Execute command
-# from subprocess import call
-# call_args = ["python", subscript]
-# call_args.extend(sys.argv)
-# call(call_args)
 
 ##
 # End of File
