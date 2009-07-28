d3de90edb2ef458141841a6c03aa50ae945e2e21
Mark Friedenbach <mfrieden@odin000956054.(none)>
2009-07-28 11:50:18 -0700

    added an error message if django module is not found; cleaned up and expanded upon existing error message

--- templates/project/manage.py
+++ templates/project/manage.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 ##
-# project.manage
+# manage.py
 ##
 
 # Added by Mark Friedenbach 15 Jul 2009
@@ -11,12 +11,28 @@ import sys
 sys.path.insert(0, '.')
 sys.path.insert(0, 'project')
 
-from django.core.management import execute_manager
+try:
+    from django.core.management import execute_manager
+except:
+    sys.stderr.write("""
+error: Can't find the django.core.management module.  Are you sure that you
+error: added Django to your installed python path, or a link to it in your
+error: project's root directory?
+""".lstrip())
+    sys.exit(1)
+
 try:
     import settings # Assumed to be in the project directory.
 except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("""
+error: Can't find the file 'settings.py' in the directory containing
+error: %r.
+error:
+error: It appears you've customized things.  You'll have to find and run
+error: django-admin.py manually, passing it your settings module.  (If the
+error: file settings.py does indeed exist, it's causing an ImportError
+error: somehow.)
+""".lstrip() % __file__)
     sys.exit(1)
 
 if __name__ == "__main__":
