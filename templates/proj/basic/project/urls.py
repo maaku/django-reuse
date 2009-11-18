5e9fc05bdbe83d6113b5164472c162b8fa374d99
Mark Friedenbach <mfrieden@odin000961417.arc.nasa.gov>
2009-11-18 15:12:19 -0800

    Moved basic template to AGPLv3 license by default.

--- templates/proj/basic/project/urls.py
+++ templates/proj/basic/project/urls.py
@@ -5,6 +5,27 @@
 # project.urls
 ##
 
+##
+# Copyright (C) $YEAR$, $AUTHOR_NAME$ <$AUTHOR_EMAIL$>
+#
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of version 3 of the GNU Affero General Public License as
+#   published by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this source code; if not, see <http://www.gnu.org/licenses/>,
+#   or write to
+#
+#     Free Software Foundation, Inc.
+#     51 Franklin Street, Fifth Floor
+#     Boston, MA  02110-1301  USA
+##
+
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
