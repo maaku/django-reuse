b8750cac1326df6ea1ea30c42cca1a7f479d154b
Mark Friedenbach <mfrieden@odin000961417.arc.nasa.gov>
2009-11-18 14:27:40 -0800

    Moved to AGPLv3 for license.

--- bin/manage.py
+++ bin/manage.py
@@ -5,6 +5,27 @@
 # django-reuse: bin/manage.py
 ##
 
+##
+# Copyright © by its contributors (see: COPYRIGHT.txt).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU Affero General Public
+# License as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this source code; if not, see <http://www.gnu.org/licenses/>
+# or write to
+#
+#   Free Software Foundation, Inc.
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA  02110-1301  USA
+##
+
 """
 An interface to django-admin.py which adds capability to manage reusable
 django applications, application dependencies, projects which use reusable
